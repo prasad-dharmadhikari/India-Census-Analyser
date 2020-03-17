701ae0e9ce1479d058b3cf788fa09fef42a355b1
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-17 12:04:45 +0530

    ADDED:Class to throw custom exceptions

---
+++ src/main/java/com/bridgelabz/censusanalyser/exception/CensusAnalyserException.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.censusanalyser.exception;
+
+public class CensusAnalyserException extends Exception
+{
+    public enum ExceptionType
+    {
+        ENTERED_WRONG_FILE_NAME;
+    }
+    public ExceptionType type;
+    // Constructor
+    public CensusAnalyserException(ExceptionType type, String message)
+    {
+        super(message);
+        this.type = type;
+    }
+}
