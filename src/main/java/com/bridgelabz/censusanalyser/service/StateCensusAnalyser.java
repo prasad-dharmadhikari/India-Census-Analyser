8ab38ec8ecf5c7dceaa5590abb75139ceba4ed0a
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-17 11:13:33 +0530

    ADDED:Test case 1.1 Given the state census analyser file,check if no of records matches

--- src/main/java/com/bridgelabz/censusanalyser/service/StateCensusAnalyser.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.censusanalyser.service;
-
-public class StateCensusAnalyser
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Welcome to Census analyser problem");
-    }
-}
---
+++ src/test/java/com/bridgelabz/censusanalyser/serviceTest/StateCensusAnalyserTest.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.censusanalyser.serviceTest;
+
+import com.bridgelabz.censusanalyser.serviceTest.service.StateCensusAnalyser;
+import com.opencsv.exceptions.CsvException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class StateCensusAnalyserTest
+{
+    private static final String PATH_OF_CSV_FILE = "./src/test/resources/StateCensusData.csv";
+    @Test
+    public void givenTheStateCensusCSVFile_WhenProper_CheckIfNoOfRecordsMatches() throws IOException, CsvException, IOException
+    {
+        int noOfRecords = StateCensusAnalyser.loadCSVFileData(PATH_OF_CSV_FILE);
+        Assert.assertEquals(28,noOfRecords);
+    }
+}
