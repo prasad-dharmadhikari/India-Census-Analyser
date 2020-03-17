dbe640b523ab00d5c8a9f61f7d003a32ee8e057f
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-17 16:59:07 +0530

    ADDED:Test case 1.3 Given State census analyser file if type is incorrect should throw custom exception

--- src/test/java/com/bridgelabz/censusanalyser/serviceTest/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/censusanalyser/serviceTest/StateCensusAnalyserTest.java
@@ -6,12 +6,14 @@ import com.opencsv.exceptions.CsvException;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 
 public class StateCensusAnalyserTest
 {
     private static final String PATH_OF_CSV_FILE = "./src/test/resources/StateCensusData.csv";
-    private static final String PATH_OF_CSV_FILE_FOR_EXCEPTION = "./src/test/resources/StateCensu.csv";
+    private static final String PATH_OF_CSV_FILE_FOR_FILE_NOT_FOUND_EXCEPTION = "./src/test/resources/StateCensu.csv";
+    private static final String PATH_OF_CSV_FILE_FOR_INCORRECT_TYPE_EXCEPTION = "/home/bridgelabz/Desktop/IndiaCensusAnalyser/src/test/resources/StateCensusData.docx";
 
     @Test
     public void givenTheStateCensusCSVFile_WhenProper_CheckIfNoOfRecordsMatches() throws IOException, CsvException, IOException, CensusAnalyserException
@@ -20,15 +22,27 @@ public class StateCensusAnalyserTest
         Assert.assertEquals(28,noOfRecords);
     }
     @Test
-    public void givenTheStateCensusCSVFile_IfIncorrect_ShouldThrowCustomException()
+    public void givenTheStateCensusCSVFile_IfIncorrect_ShouldThrowCustomException() throws IOException
     {
         try
         {
-            int noOfRecords = StateCensusAnalyser.loadCSVFileData(PATH_OF_CSV_FILE_FOR_EXCEPTION);
+            int noOfRecords = StateCensusAnalyser.loadCSVFileData(PATH_OF_CSV_FILE_FOR_FILE_NOT_FOUND_EXCEPTION);
         }
         catch (CensusAnalyserException e)
         {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.ENTERED_WRONG_FILE_NAME,e.type);
+        }
+    }
+    @Test
+    public void givenTheStateCensusCSVFile_IfTypeIsIncorrect_ShouldThrowCustomException() throws CensusAnalyserException, IOException
+    {
+        try
+        {
+            StateCensusAnalyser.getFileExtension(new File(PATH_OF_CSV_FILE_FOR_INCORRECT_TYPE_EXCEPTION));
+        }
+        catch (CensusAnalyserException e)
+        {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.ENTERED_WRONG_FILE_TYPE,e.type);
         }
     }
 }
