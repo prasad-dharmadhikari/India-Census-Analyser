14142f752592e6afb02812e05be6a5245ce3f82c
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-17 10:41:02 +0530

    ADDED:Welcome message

---
+++ src/main/java/com/bridgelabz/censusanalyser/service/StateCensusAnalyser.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.censusanalyser.service;
+
+public class StateCensusAnalyser
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Census analyser problem");
+    }
+}
