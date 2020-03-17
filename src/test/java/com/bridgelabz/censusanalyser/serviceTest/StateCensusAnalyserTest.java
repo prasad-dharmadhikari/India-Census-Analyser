48df275a6c2ac9848012a66ff8dd5f4843098b7b
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-17 12:08:10 +0530

    ADDED:Test case 1.2 Given the state census analyser file, when incorrect should throw custom exception

--- src/test/java/com/bridgelabz/censusanalyser/serviceTest/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/censusanalyser/serviceTest/StateCensusAnalyserTest.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.censusanalyser.serviceTest;
 
+import com.bridgelabz.censusanalyser.exception.CensusAnalyserException;
 import com.bridgelabz.censusanalyser.service.StateCensusAnalyser;
 import com.opencsv.exceptions.CsvException;
 import org.junit.Assert;
@@ -10,10 +11,24 @@ import java.io.IOException;
 public class StateCensusAnalyserTest
 {
     private static final String PATH_OF_CSV_FILE = "./src/test/resources/StateCensusData.csv";
+    private static final String PATH_OF_CSV_FILE_FOR_EXCEPTION = "./src/test/resources/StateCensu.csv";
+
     @Test
-    public void givenTheStateCensusCSVFile_WhenProper_CheckIfNoOfRecordsMatches() throws IOException, CsvException, IOException
+    public void givenTheStateCensusCSVFile_WhenProper_CheckIfNoOfRecordsMatches() throws IOException, CsvException, IOException, CensusAnalyserException
     {
         int noOfRecords = StateCensusAnalyser.loadCSVFileData(PATH_OF_CSV_FILE);
         Assert.assertEquals(28,noOfRecords);
+    }
+    @Test
+    public void givenTheStateCensusCSVFile_IfIncorrect_ShouldThrowCustomException()
+    {
+        try
+        {
+            int noOfRecords = StateCensusAnalyser.loadCSVFileData(PATH_OF_CSV_FILE_FOR_EXCEPTION);
+        }
+        catch (CensusAnalyserException e)
+        {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.ENTERED_WRONG_FILE_NAME,e.type);
+        }
     }
 }
