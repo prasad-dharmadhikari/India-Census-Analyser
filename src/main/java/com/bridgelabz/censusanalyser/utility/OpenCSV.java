f3f21a394736a485fbbe5f3fe8f0c44b0b9dc2e6
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-22 20:33:50 +0530

    REFACTORED:Refactor 2 code to ensure single responsibility for analyser and use delegation principle to handle CSV files

---
+++ src/main/java/com/bridgelabz/censusanalyser/utility/OpenCSV.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.censusanalyser.utility;
+
+import com.bridgelabz.censusanalyser.exception.CensusAnalyserException;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.Reader;
+import java.util.Iterator;
+
+public class OpenCSV
+{
+    public static <E> Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CensusAnalyserException
+    {
+        try
+        {
+            CSVReader csvReader = new CSVReader(reader);
+            CsvToBean<E> csvToBean = new CsvToBeanBuilder<E>(csvReader).withType(csvClass).build();
+            return csvToBean.iterator();
+        }
+        catch (IllegalStateException e)
+        {
+            throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE, e.getMessage());
+        }
+    }
+    public static <E> int getCount(Iterator<E> csvRecords)
+    {
+        int noOfRecords = 0;
+        while (csvRecords.hasNext())
+        {
+            noOfRecords++;
+            csvRecords.next();
+        }
+        return noOfRecords;
+    }
+}
