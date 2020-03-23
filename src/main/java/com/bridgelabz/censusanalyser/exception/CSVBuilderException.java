9ecc13f7887cd45472b9410539622503db0f227a
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-23 20:25:51 +0530

    REFACTORED:NO_CENSUS_DATA as constant in enum

--- src/main/java/com/bridgelabz/censusanalyser/exception/CSVBuilderException.java
+++ src/main/java/com/bridgelabz/censusanalyser/exception/CSVBuilderException.java
@@ -5,7 +5,7 @@ public class CSVBuilderException extends Exception
     public enum ExceptionType
     {
         ENTERED_WRONG_FILE_NAME,ENTERED_WRONG_FILE_TYPE,
-        INCORRECT_DELIMITER_OR_HEADER,UNABLE_TO_PARSE;
+        INCORRECT_DELIMITER_OR_HEADER,UNABLE_TO_PARSE, NO_CENSUS_DATA;
     }
     public ExceptionType type;
     // Constructor
