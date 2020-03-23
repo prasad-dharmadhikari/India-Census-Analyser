a91105e1740b4ea97fe2f774cf7d5d1ffd31a58f
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-23 17:16:15 +0530

    REFACTORED:Refactor 5 Code to take in list of census data

--- src/main/java/com/bridgelabz/censusanalyser/utility/CSVBuilder.java
+++ src/main/java/com/bridgelabz/censusanalyser/utility/CSVBuilder.java
@@ -5,33 +5,22 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import java.io.Reader;
-import java.util.Iterator;
+import java.util.List;
 
 public class CSVBuilder implements ICSVBuilder
 {
     @Override
-    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CSVBuilderException
+    public <E> List<E> getCSVFileList(Reader reader, Class csvClass) throws CSVBuilderException
     {
         try
         {
             CSVReader csvReader = new CSVReader(reader);
             CsvToBean<E> csvToBean = new CsvToBeanBuilder<E>(csvReader).withType(csvClass).build();
-            return csvToBean.iterator();
+            return csvToBean.parse();
         }
         catch (IllegalStateException e)
         {
             throw new CSVBuilderException(CSVBuilderException.ExceptionType.UNABLE_TO_PARSE, e.getMessage());
         }
-    }
-    @Override
-    public <E> int getCount(Iterator<E> csvRecords)
-    {
-        int noOfRecords = 0;
-        while (csvRecords.hasNext())
-        {
-            noOfRecords++;
-            csvRecords.next();
-        }
-        return noOfRecords;
     }
 }
--- src/main/java/com/bridgelabz/censusanalyser/utility/ICSVBuilder.java
+++ src/main/java/com/bridgelabz/censusanalyser/utility/ICSVBuilder.java
@@ -2,10 +2,9 @@ package com.bridgelabz.censusanalyser.utility;
 
 import com.bridgelabz.censusanalyser.exception.CSVBuilderException;
 import java.io.Reader;
-import java.util.Iterator;
+import java.util.List;
 
 public interface ICSVBuilder
 {
-    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CSVBuilderException;
-    public <E> int getCount(Iterator<E> csvRecords);
+    public <E> List<E> getCSVFileList(Reader reader, Class csvClass) throws CSVBuilderException;
 }
