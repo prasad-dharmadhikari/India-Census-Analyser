4f131ff29632f1c3df6b4c60b1ec08b484276274
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-26 17:33:26 +0530

    REFACTORED:Refactor 6 code to store the csv file data to Hashmap

--- src/main/java/com/bridgelabz/censusanalyser/service/CensusAnalyser.java
+++ src/main/java/com/bridgelabz/censusanalyser/service/CensusAnalyser.java
@@ -12,21 +12,21 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 import com.google.gson.Gson;
 
 public class CensusAnalyser {
     ICSVBuilder csvBuilder = new CSVBuilderFactory().createCSVBuilder();
-    List<CSVStateCensus> stateCensusRecords = null;
-    List<StateCode> stateCodeRecords = null;
+    Collection<Object> stateCensusRecords = null;
+    Collection<Object> stateCodeRecords = null;
+    HashMap<Object, Object> stateCodeHashMap = null;
+    HashMap<Object, Object> stateCensusHashMap = null;
 
     public int loadStateCensusCSVFileData(String filePath) throws CSVBuilderException, IOException {
         try (Reader reader = Files.newBufferedReader(Paths.get(filePath))) {
-            stateCensusRecords = csvBuilder.getCSVFileList(reader, CSVStateCensus.class);
-            return stateCensusRecords.size();
+            stateCensusHashMap = csvBuilder.getCSVFileMap(reader, CSVStateCensus.class);
+            return stateCensusHashMap.size();
         } catch (NoSuchFileException e) {
             throw new CSVBuilderException(CSVBuilderException.ExceptionType.ENTERED_WRONG_FILE_NAME,
                     "FILE NAME IS INCORRECT");
@@ -38,8 +38,8 @@ public class CensusAnalyser {
 
     public int loadStateCodeCSVFileData(String filePath) throws IOException, CSVBuilderException {
         try (Reader reader = Files.newBufferedReader(Paths.get(filePath))) {
-            stateCodeRecords = csvBuilder.getCSVFileList(reader, StateCode.class);
-            return stateCodeRecords.size();
+            stateCodeHashMap = csvBuilder.getCSVFileMap(reader, StateCode.class);
+            return stateCodeHashMap.size();
         } catch (NoSuchFileException e) {
             throw new CSVBuilderException(CSVBuilderException.ExceptionType.ENTERED_WRONG_FILE_NAME,
                     "FILE NAME IS INCORRECT");
@@ -62,37 +62,36 @@ public class CensusAnalyser {
     }
 
     public String getStateWiseSortedData() throws CSVBuilderException {
-        if (stateCensusRecords == null || stateCensusRecords.size() == 0)
+        if (stateCensusHashMap == null || stateCensusHashMap.size() == 0)
             throw new CSVBuilderException(CSVBuilderException.ExceptionType.NO_CENSUS_DATA, "Data empty");
-
-        Comparator<CSVStateCensus> censusCSVComparator = Comparator.comparing(csvStateCensus ->
-                csvStateCensus.State);
-        this.sort(censusCSVComparator , stateCensusRecords);
+        Comparator<CSVStateCensus> censusCSVComparator = Comparator.comparing(csvStateCensus -> csvStateCensus.State);
+        this.sort(censusCSVComparator , stateCensusHashMap);
+        stateCensusRecords = stateCensusHashMap.values();
         String sortedStateCensusJson = new Gson().toJson(stateCensusRecords);
         return sortedStateCensusJson;
     }
 
-    public <E> void sort(Comparator<E> censusCSVComparator , List censusRecords) {
+    public String getStateCodeWiseSortedData() throws CSVBuilderException {
+        if (stateCodeHashMap == null || stateCodeHashMap.size() == 0)
+            throw new CSVBuilderException(CSVBuilderException.ExceptionType.NO_CENSUS_DATA, "Data empty");
+        Comparator<StateCode> stateCodeCSVComparator = Comparator.comparing(stateCode -> stateCode.stateCode);
+        this.sort(stateCodeCSVComparator , stateCodeHashMap);
+        stateCodeRecords = stateCodeHashMap.values();
+        String sortedStateCodeJson = new Gson().toJson(stateCodeRecords);
+        return sortedStateCodeJson;
+    }
+
+    public <E> void sort(Comparator<E> censusCSVComparator , HashMap<Object, Object> censusRecords) {
         for (int iterate = 0; iterate < censusRecords.size() - 1; iterate++) {
             for (int Inneriterate = 0; Inneriterate < censusRecords.size() - iterate - 1; Inneriterate++) {
                 E census1 = (E) censusRecords.get(Inneriterate);
                 E census2 = (E) censusRecords.get(Inneriterate + 1);
                 if (censusCSVComparator.compare(census1, census2) > 0) {
-                    censusRecords.set(Inneriterate, census2);
-                    censusRecords.set(Inneriterate + 1, census1);
+                    censusRecords.put(Inneriterate, census2);
+                    censusRecords.put(Inneriterate + 1, census1);
                 }
             }
         }
     }
 
-    public String getStateCodeWiseSortedData() throws CSVBuilderException {
-        if (stateCodeRecords == null || stateCodeRecords.size() == 0)
-            throw new CSVBuilderException(CSVBuilderException.ExceptionType.NO_CENSUS_DATA, "Data empty");
-
-        Comparator<StateCode> stateCodeCSVComparator = Comparator.comparing(stateCode ->
-                stateCode.stateCode);
-        this.sort(stateCodeCSVComparator , stateCodeRecords);
-        String sortedStateCodeJson = new Gson().toJson(stateCodeRecords);
-        return sortedStateCodeJson;
-    }
 }
