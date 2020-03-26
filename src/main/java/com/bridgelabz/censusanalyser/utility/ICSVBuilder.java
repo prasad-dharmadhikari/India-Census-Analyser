e8d522c14f115cd266452ecd68bbaaafbcaacdaf
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-26 17:36:10 +0530

    REFACTORED:Changed the method name to getCSVFileMap

--- src/main/java/com/bridgelabz/censusanalyser/utility/ICSVBuilder.java
+++ src/main/java/com/bridgelabz/censusanalyser/utility/ICSVBuilder.java
@@ -1,9 +1,10 @@
 package com.bridgelabz.censusanalyser.utility;
 
 import com.bridgelabz.censusanalyser.exception.CSVBuilderException;
+
 import java.io.Reader;
-import java.util.List;
+import java.util.HashMap;
 
 public interface ICSVBuilder {
-    public <E> List<E> getCSVFileList(Reader reader, Class csvClass) throws CSVBuilderException;
+    public <E> HashMap<E, E> getCSVFileMap(Reader reader, Class csvClass) throws CSVBuilderException;
 }
