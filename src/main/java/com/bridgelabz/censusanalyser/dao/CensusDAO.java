fd935837c3f7536954e58d83c5cec2132c58d362
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-27 16:28:25 +0530

    REFACTORED:Renamed file to CensusDAO and added constructor of USCensus POJO

--- src/main/java/com/bridgelabz/censusanalyser/dao/CensusDAO.java
+++ src/main/java/com/bridgelabz/censusanalyser/dao/CensusDAO.java
@@ -2,27 +2,36 @@ package com.bridgelabz.censusanalyser.dao;
 
 import com.bridgelabz.censusanalyser.model.CSVStateCensus;
 import com.bridgelabz.censusanalyser.model.StateCode;
+import com.bridgelabz.censusanalyser.model.USCensus;
 
-public class IndiaCensusDAO {
+public class CensusDAO {
     public String state;
     public long population;
     public long AreaInSqKm;
     public int DensityPerSqkm;
+    public double area;
+    public float density;
     public String stateCode;
     public int tin;
     public int srNo;
 
-    public IndiaCensusDAO(StateCode stateCode) {
+    public CensusDAO(StateCode stateCode) {
         this.state = stateCode.stateName;
         this.srNo = stateCode.srNo;
         this.tin = stateCode.tin;
         this.stateCode = stateCode.stateCode;
     }
-    public IndiaCensusDAO(CSVStateCensus csvStateCensus) {
+    public CensusDAO(CSVStateCensus csvStateCensus) {
         this.state = csvStateCensus.State;
         this.population = csvStateCensus.Population;
         this.AreaInSqKm = csvStateCensus.AreaInSqKm;
         this.DensityPerSqkm = csvStateCensus.DensityPerSqkm;
     }
-
+    public CensusDAO(USCensus usCensus) {
+        this.state = usCensus.state;
+        this.population = usCensus.population;
+        area = usCensus.area;
+        density = usCensus.populationDensity;
+        this.stateCode = usCensus.stateID;
+    }
 }
