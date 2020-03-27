9d0b321468dbdf84abd9bc0c653e9fbe393c9249
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-27 16:29:27 +0530

    ADDED:POJO class for USCensus csv file

--- src/main/java/com/bridgelabz/censusanalyser/model/USCensus.java
+++ src/main/java/com/bridgelabz/censusanalyser/model/USCensus.java
@@ -1,4 +1,42 @@
 package com.bridgelabz.censusanalyser.model;
 
+import com.opencsv.bean.CsvBindByName;
+
 public class USCensus {
+    @CsvBindByName(column = "State Id")
+    public String stateID;
+
+    @CsvBindByName(column = "State")
+    public String state;
+
+    @CsvBindByName(column = "Population")
+    public long population;
+
+    @CsvBindByName(column = "Total area")
+    public double area;
+
+    @CsvBindByName(column = "Population Density")
+    public float populationDensity;
+
+    public USCensus() {
+    }
+
+    public USCensus(String stateID, String state, long population, double area, float populationDensity) {
+        this.stateID = stateID;
+        this.state = state;
+        this.population = population;
+        this.area = area;
+        this.populationDensity = populationDensity;
+    }
+
+    @Override
+    public String toString() {
+        return "USCensus{" +
+                "stateID='" + stateID + '\'' +
+                ", state='" + state + '\'' +
+                ", population=" + population +
+                ", area=" + area +
+                ", populationDensity='" + populationDensity + '\'' +
+                '}';
+    }
 }
