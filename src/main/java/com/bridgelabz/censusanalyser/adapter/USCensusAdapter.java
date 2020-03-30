cb6ec8bf977e581c89fcc67f6cb998c149150d23
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-30 22:59:21 +0530

    ADDED:Created IndiaCensusAdapter class to load US census data

--- src/main/java/com/bridgelabz/censusanalyser/adapter/USCensusAdapter.java
+++ src/main/java/com/bridgelabz/censusanalyser/adapter/USCensusAdapter.java
@@ -1,4 +1,18 @@
 package com.bridgelabz.censusanalyser.adapter;
 
-public class USCensusAdapter {
+import com.bridgelabz.censusanalyser.dao.CensusDAO;
+import com.bridgelabz.censusanalyser.exception.CSVBuilderException;
+import com.bridgelabz.censusanalyser.model.USCensus;
+import com.bridgelabz.censusanalyser.utility.CSVBuilderFactory;
+import com.bridgelabz.censusanalyser.utility.ICSVBuilder;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class USCensusAdapter extends CensusAdapter {
+
+    @Override
+    public HashMap<Integer, CensusDAO> loadCensusData(String... csvFilePath) throws CSVBuilderException, IOException {
+        return super.loadCensusData(USCensus.class, csvFilePath[0]);
+    }
 }
