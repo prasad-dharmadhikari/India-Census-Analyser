8e9ccc880bf246950ff31050e41e8aed29eb7071
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-30 22:57:12 +0530

    ADDED:Created CensusAdapterFactory to return required adapter class object

--- src/main/java/com/bridgelabz/censusanalyser/adapter/CensusAdapterFactory.java
+++ src/main/java/com/bridgelabz/censusanalyser/adapter/CensusAdapterFactory.java
@@ -1,4 +1,13 @@
 package com.bridgelabz.censusanalyser.adapter;
 
+import com.bridgelabz.censusanalyser.service.CensusAnalyser;
+
 public class CensusAdapterFactory {
+    public static CensusAdapter getCensusData(CensusAnalyser.COUNTRY country) {
+        if (country.equals(CensusAnalyser.COUNTRY.INDIA))
+            return new IndiaCensusAdapter();
+        if (country.equals(CensusAnalyser.COUNTRY.US))
+            return new USCensusAdapter();
+        return null;
+    }
 }
