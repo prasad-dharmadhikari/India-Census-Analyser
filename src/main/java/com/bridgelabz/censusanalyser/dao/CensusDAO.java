c1e31848b4f7b10319feb62d3db94c64ca27b6ed
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-01 15:58:15 +0530

    REFACTORED:Provision for returning custom comparators and POJO specific DTOs

--- src/main/java/com/bridgelabz/censusanalyser/dao/CensusDAO.java
+++ src/main/java/com/bridgelabz/censusanalyser/dao/CensusDAO.java
@@ -1,41 +1,76 @@
 package com.bridgelabz.censusanalyser.dao;
 
-import com.bridgelabz.censusanalyser.model.CSVStateCensus;
-import com.bridgelabz.censusanalyser.model.StateCode;
+import com.bridgelabz.censusanalyser.model.IndiaStateCensusCSV;
+import com.bridgelabz.censusanalyser.model.IndiaStateCodeCSV;
 import com.bridgelabz.censusanalyser.model.USCensus;
+import com.bridgelabz.censusanalyser.service.CensusAnalyser;
+
+import java.util.Comparator;
 
 public class CensusDAO {
 
+    public Integer tin;
+    public Integer srNo;
     public String state;
-    public long population;
-    public long AreaInSqKm;
-    public int DensityPerSqkm;
-    public double area;
-    public float density;
     public String stateCode;
-    public int tin;
-    public int srNo;
+    public Double AreaInSqKm;
+    public Double DensityPerSqkm;
+    public Integer population;
 
-    public CensusDAO(StateCode stateCode) {
-        this.state = stateCode.stateName;
-        this.srNo = stateCode.srNo;
-        this.tin = stateCode.tin;
-        this.stateCode = stateCode.stateCode;
+
+    public CensusDAO(IndiaStateCodeCSV indiaStateCodeCSV) {
+        this.state = indiaStateCodeCSV.stateName;
+        this.srNo = indiaStateCodeCSV.srNo;
+        this.tin = indiaStateCodeCSV.tin;
+        this.stateCode = indiaStateCodeCSV.stateCode;
     }
 
-    public CensusDAO(CSVStateCensus csvStateCensus) {
-        this.state = csvStateCensus.State;
-        this.population = csvStateCensus.Population;
-        this.AreaInSqKm = csvStateCensus.AreaInSqKm;
-        this.DensityPerSqkm = csvStateCensus.DensityPerSqkm;
+    public CensusDAO(IndiaStateCensusCSV indiaStateCensusCSV) {
+        this.state = indiaStateCensusCSV.State;
+        this.population = indiaStateCensusCSV.Population;
+        this.AreaInSqKm = indiaStateCensusCSV.AreaInSqKm;
+        this.DensityPerSqkm = indiaStateCensusCSV.DensityPerSqkm;
     }
 
     public CensusDAO(USCensus usCensus) {
         this.state = usCensus.state;
         this.population = usCensus.population;
-        area = usCensus.area;
-        density = usCensus.populationDensity;
+        this.AreaInSqKm = usCensus.area;
+        this.DensityPerSqkm = usCensus.populationDensity;
         this.stateCode = usCensus.stateID;
     }
 
+    public Double getAreaInSqKm() {
+        return AreaInSqKm;
+    }
+
+    public Double getDensityPerSqkm() {
+        return DensityPerSqkm;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
+
+    public static Comparator<? super CensusDAO> getSortComparator(CensusAnalyser.SortingMode mode) {
+        if (mode.equals(CensusAnalyser.SortingMode.STATE))
+            return Comparator.comparing(census -> census.state);
+        if (mode.equals(CensusAnalyser.SortingMode.STATECODE))
+            return Comparator.comparing(census -> census.stateCode);
+        if (mode.equals(CensusAnalyser.SortingMode.POPULATION))
+            return Comparator.comparing(CensusDAO::getPopulation).reversed();
+        if (mode.equals(CensusAnalyser.SortingMode.DENSITY))
+            return Comparator.comparing(CensusDAO::getDensityPerSqkm).reversed();
+        if (mode.equals(CensusAnalyser.SortingMode.AREA))
+            return Comparator.comparing(CensusDAO::getAreaInSqKm).reversed();
+        return null;
+    }
+
+    public Object getCensusDTO(CensusAnalyser.COUNTRY country) {
+        if (country.equals(CensusAnalyser.COUNTRY.INDIA))
+            return new IndiaStateCensusCSV(state, stateCode, population, AreaInSqKm, DensityPerSqkm);
+        if (country.equals(CensusAnalyser.COUNTRY.US))
+            return new USCensus(stateCode, state, population, AreaInSqKm, DensityPerSqkm);
+        return null;
+    }
 }
